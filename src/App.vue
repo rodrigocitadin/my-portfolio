3dccd4998ea382ea5f42b5a08860468c9e07f7e5
rodrigocitadin <citadinrodrigo@gmail.com>
2022-11-19 23:19:51 -0300

    feat: Added my profile picture

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyPictureVue />
+  <MyButtonsVue />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyPictureVue from './components/MyPicture.vue';
+import MyButtonsVue from './components/MyButtons.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyPictureVue,
+    MyButtonsVue
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  background: #202020;
 }
 </style>
---
+++ src/components/MyPicture.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="my-pic">
+    <img src="../assets/my-picture.png" alt="Citadin's picture" id="citadin-pic">
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.my-pic {
+  display: flex;
+  justify-content: center;
+  margin: 60px 0;
+}
+
+#citadin-pic {
+  width: 230px;
+  background-color: white;
+  border-radius: 100%;
+}
+</style>
