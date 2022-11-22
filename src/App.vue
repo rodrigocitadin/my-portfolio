a57aaf7c17522589079edeb4e9b7b7b5a731f98f
rodrigocitadin <citadinrodrigo@gmail.com>
2022-11-22 00:22:06 -0300

    feat: Added some effects and styles

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <MyPictureVue />
-  <div class="my-buttons">
-    <a target="_blank" :href="button.link" v-for="button in buttons" :key="button.id">
-      <button>{{ button.msg }}</button>
-    </a>
+  <MyHeaderVue />
+  <div class="buttons">
+    <button class="button" v-for="button in buttons" :key="button.id" target="_blank">
+      <a :href="button.link">{{ button.msg }}</a>
+    </button>
   </div>
 </template>
 
 <script>
-import MyPictureVue from './components/MyPicture.vue';
+import MyHeaderVue from './components/MyHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    MyPictureVue
+    MyHeaderVue
   },
   data() {
     return {
@@ -21,22 +21,26 @@ export default {
         {
           id: 1,
           msg: 'ABOUT ME',
-          link: ''
+          link: '',
+          color: '#adadad'
         },
         {
           id: 2,
           msg: 'LINKEDIN',
-          link: 'https://www.linkedin.com/in/rodrigobcitadin/'
+          link: 'https://www.linkedin.com/in/rodrigobcitadin/',
+          color: '#01649b'
         },
         {
           id: 3,
           msg: 'GITHUB',
-          link: 'https://github.com/rodrigocitadin'
+          link: 'https://github.com/rodrigocitadin',
+          color: '#6d2a89'
         },
         {
           id: 4,
           msg: 'DEV.TO',
-          link: 'https://dev.to/rodrigocitadin'
+          link: 'https://dev.to/rodrigocitadin',
+          color: '#3ec9b4'
         }
       ]
     }
@@ -45,12 +49,39 @@ export default {
 </script>
 
 <style>
+* {
+  font-family: 'JetBrains Mono', monospace;
+}
+
 html {
+  height: 100%;
+  width: 100%;
   background: #202020;
 }
 
-.my-buttons {
+.buttons {
   display: flex;
   flex-direction: column;
+}
+
+.button {
+  width: 30rem;
+  height: 4rem;
+  margin: 0.6rem auto;
+  border-radius: 12px;
+  background-color: whitesmoke;
+  -webkit-transition: all 0.3s 0s ease-in;
+  -moz-transition: all 0.3s 0s ease-in;
+  -o-transition: all 0.3s 0s ease-in;
+  transition: all 0.3s 0s ease-in;
+}
+
+.button:hover {
+  padding-left: 20px;
+  background-color: rgb(189, 189, 189);
+}
+
+.button a {
+  font-size: 1.2rem;
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <div class="my-pic">
+      <img src="../assets/my-picture.png" alt="Citadin's picture" id="citadin-pic">
+    </div>
+    <div class="my-name">
+      <h1>Citadin</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.my-pic {
+  display: flex;
+  justify-content: center;
+  margin: 80px 0 25px 0;
+}
+
+#citadin-pic {
+  width: 230px;
+  background-color: whitesmoke;
+  border-radius: 100%;
+}
+
+.my-name {
+  color: whitesmoke;
+  margin-bottom: 120px;
+  display: flex;
+  justify-content: center;
+}
+</style>
