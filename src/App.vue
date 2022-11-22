f70863df1c7bcaee3d550a69cd76324d53ed8611
rodrigocitadin <citadinrodrigo@gmail.com>
2022-11-21 22:10:47 -0300

    Added buttons in loop

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,56 @@
 <template>
   <MyPictureVue />
-  <MyButtonsVue />
+  <div class="my-buttons">
+    <a target="_blank" :href="button.link" v-for="button in buttons" :key="button.id">
+      <button>{{ button.msg }}</button>
+    </a>
+  </div>
 </template>
 
 <script>
 import MyPictureVue from './components/MyPicture.vue';
-import MyButtonsVue from './components/MyButtons.vue';
 
 export default {
   name: 'App',
   components: {
-    MyPictureVue,
-    MyButtonsVue
-  }
+    MyPictureVue
+  },
+  data() {
+    return {
+      buttons: [
+        {
+          id: 1,
+          msg: 'ABOUT ME',
+          link: ''
+        },
+        {
+          id: 2,
+          msg: 'LINKEDIN',
+          link: 'https://www.linkedin.com/in/rodrigobcitadin/'
+        },
+        {
+          id: 3,
+          msg: 'GITHUB',
+          link: 'https://github.com/rodrigocitadin'
+        },
+        {
+          id: 4,
+          msg: 'DEV.TO',
+          link: 'https://dev.to/rodrigocitadin'
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <style>
 html {
   background: #202020;
+}
+
+.my-buttons {
+  display: flex;
+  flex-direction: column;
 }
 </style>
